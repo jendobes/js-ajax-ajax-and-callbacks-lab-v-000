318c72b2d8fe66ee511902b433aaf56881216a5f
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 21:42:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 $(document).ready(function (){
 });
 
+function searchRepositories() {
+  const input = $("#searchTerms")[0].value;
+  const url = "https://api.github.com/search/repositories?q=" + input;
+
+  $.get(url, function(data) {
+    console.log(data.items);
+
+    const repos = data.items;
+    const repoList = "<ul>" + repos.map(r => {
+      return (`
+        <li>
+        <h2><a href="${r.html_url}">${r.name}</a></h2>
+        <p>Description: ${r.description}</p>
+        <p>Created by: ${r.owner.login}</p>
+        <a href="#" onclick="showCommits(this)" data-repository="${r.name}" data-owner="${r.owner.login}">Show Commits</a>
+        <p><img src="${r.owner.avatar_url}" height="45" width="45"></p>
+        </li>
+        `)
+    }).join('') + "</ul>"
+    document.getElementById("results").innerHTML = repoList
+  }).fail(function() {
+    displayError();
+  });
+}
+
 function showCommits(el) {
   const owner = el.dataset.owner
   const repo = el.dataset.repository
