9cae4fe621f0eade3800837f9224e91e3a508c02
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 21:38:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ $(document).ready(function (){
 //   })
 // }
 
-function displayRepositories(data) {
-  const repos = data.items
-  const repoList = "<ul>" + repos.map(r => {`
-    <li>
-    <h2><a href="${r.html_url}">${r.name}</a></h2>
-    <p>Description: ${r.description}</p>
-    <p>Created by: ${r.owner.login}</p>
-    <a href="#" onclick="showCommits(this)" data-repository="${r.name}" data-owner="${r.owner.login}">Show Commits</a>
-    <p><img src="${r.owner.avatar_url}" height="45" width="45"></p>
-    </li>
-  `}).join('') + "</ul>"
-  return repoList
-}
+// function displayRepositories(data) {
+//   const repos = data.items
+//   const repoList = "<ul>" + repos.map(r => {`
+//     <li>
+//     <h2><a href="${r.html_url}">${r.name}</a></h2>
+//     <p>Description: ${r.description}</p>
+//     <p>Created by: ${r.owner.login}</p>
+//     <a href="#" onclick="showCommits(this)" data-repository="${r.name}" data-owner="${r.owner.login}">Show Commits</a>
+//     <p><img src="${r.owner.avatar_url}" height="45" width="45"></p>
+//     </li>
+//   `}).join('') + "</ul>"
+//   return repoList
+// }
 
 function showCommits(el) {
   const owner = el.dataset.owner
