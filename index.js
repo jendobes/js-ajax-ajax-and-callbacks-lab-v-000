5e95751e0088926bcf617308f2cdb4085e2bbb12
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 21:31:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,8 +25,24 @@ function displayRepositories(data) {
 }
 
 function showCommits(el) {
-  console.log(el)
   const owner = el.dataset.owner
+  const repo = el.dataset.repository
+  const url = "https://api.github.com/repos/" + owner + "/" + repo + "/commits";
+  $.get(url, function(data) {
+    const commits = data
+    const commitList = "<ul>" + commits.map(c => {
+      return (`
+        <li>
+        <p>SHA: ${c.sha}</p>
+         <p>Author: ${c.commit.author.name}</p>
+         <p>Author login: ${c.author.login}</p>
+         <p><img src="${c.author.avatar_url}" height="45" width="45"></p>
+        </li>
+        `).join('') + "</ul>"
+        document.getElementById("details").innerHTML = commitList
+    }).fail(function() {
+      displayError()
+  })
 }
 
 
