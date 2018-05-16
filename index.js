c31780c7e44714ae46fe8020bdf7c5dc7e752699
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 21:40:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ $(document).ready(function (){
 //   $("#errors").html("I'm sorry, there's been an error. Please try again.")
 // }
 
-// function searchRepositories(){
-//   const searchTerms = $("#searchTerms").val()
-//   $.get(` https://api.github.com/search/repositories?q=${searchTerms}`, function(data){
-//     $('#results').html(displayRepositories(data))
-//   }).fail(function(error) {
-//     displayError()
-//   })
-// }
+function searchRepositories(){
+  const searchTerms = $("#searchTerms").val()
+  $.get(` https://api.github.com/search/repositories?q=${searchTerms}`, function(data){
+    $('#results').html(displayRepositories(data))
+  }).fail(function(error) {
+    displayError()
+  })
+}
 
-// function displayRepositories(data) {
-//   const repos = data.items
-//   const repoList = "<ul>" + repos.map(r => {`
-//     <li>
-//     <h2><a href="${r.html_url}">${r.name}</a></h2>
-//     <p>Description: ${r.description}</p>
-//     <p>Created by: ${r.owner.login}</p>
-//     <a href="#" onclick="showCommits(this)" data-repository="${r.name}" data-owner="${r.owner.login}">Show Commits</a>
-//     <p><img src="${r.owner.avatar_url}" height="45" width="45"></p>
-//     </li>
-//   `}).join('') + "</ul>"
-//   return repoList
-// }
+function displayRepositories(data) {
+  const repos = data.items
+  const repoList = "<ul>" + repos.map(r => {`
+    <li>
+    <h2><a href="${r.html_url}">${r.name}</a></h2>
+    <p>Description: ${r.description}</p>
+    <p>Created by: ${r.owner.login}</p>
+    <a href="#" onclick="showCommits(this)" data-repository="${r.name}" data-owner="${r.owner.login}">Show Commits</a>
+    <p><img src="${r.owner.avatar_url}" height="45" width="45"></p>
+    </li>
+  `}).join('') + "</ul>"
+  return repoList
+}
 
 // function showCommits(el) {
 //   const owner = el.dataset.owner
