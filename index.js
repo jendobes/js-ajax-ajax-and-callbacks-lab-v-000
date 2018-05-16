88981ec9e541492ba2052f4db272f7604cc235bc
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 20:44:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,17 @@ $(document).ready(function (){
 
 function searchRepositories(){
   const searchTerms = $("#searchTerms").val()
+  $.get(` https://api.github.com/search/repositories?q=${searchTerms}`, function(data){
+    displayRepositories(data)
+  }).fail(function(error) {
+    displayError()
+  })
+}
+
+function displayRepositories() {
+
+}
+
+function showCommits() {
+
 }
