06cb2db3543d5db0edb46fda25e3c2fcf94e508d
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 21:38:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ $(document).ready(function (){
 //   $("#errors").html("I'm sorry, there's been an error. Please try again.")
 // }
 
-function searchRepositories(){
-  const searchTerms = $("#searchTerms").val()
-  $.get(` https://api.github.com/search/repositories?q=${searchTerms}`, function(data){
-    $('#results').html(displayRepositories(data))
-  }).fail(function(error) {
-    displayError()
-  })
-}
+// function searchRepositories(){
+//   const searchTerms = $("#searchTerms").val()
+//   $.get(` https://api.github.com/search/repositories?q=${searchTerms}`, function(data){
+//     $('#results').html(displayRepositories(data))
+//   }).fail(function(error) {
+//     displayError()
+//   })
+// }
 
 function displayRepositories(data) {
   const repos = data.items
