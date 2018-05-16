851e59f09ea81e8a205ed9abbd33eb13bc141f0d
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 21:37:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 $(document).ready(function (){
 });
 
-function displayError() {
-  $("#errors").html("I'm sorry, there's been an error. Please try again.")
-}
+// function displayError() {
+//   $("#errors").html("I'm sorry, there's been an error. Please try again.")
+// }
 
 function searchRepositories(){
   const searchTerms = $("#searchTerms").val()
